902b236a4cbcd575c4ef5951e2bcf6c5d094c2c7
sumaiyamoin <sumaiyamoin@gmaul.com>
2025-04-26 23:32:39 +0600

    apply oop

--- pages/signup.ts
+++ pages/signup.ts
@@ -4,42 +4,36 @@ class Signup {
         constructor(page: Page) {
                 this.page = page;
         }
-        registration(name:string, email:string) {
-                this.page.getByText('Signup').click();
-                this.page.getByRole('textbox', { name: 'Name' }).fill(name);
-                this.page.getByRole('textbox', { name: 'Email Address'}).fill(email);
-                //this.// page.getByRole('textbox',{name:"Email Address"}).fill("testuser005@yopmail.com");
-                this.page.getByRole('button', { name: 'Signup' }).click();
-                this.page.getByRole('radio', { name: 'Mrs.' }).click();
-                this.page.getByRole('textbox', { name: 'Password' }).fill('testuser123');
-                //this.// page.getByRole('combobox',{value:'Day'}).click();
-                //this.// page.getByText("Day").click();
-                this.page.locator('#days').nth(0).click();
-                this.page.keyboard.press("ArrowDown");
-                this.page.keyboard.press("Enter");
-                this.page.locator('#months').nth(0).click();
-                this.page.keyboard.press("ArrowDown");
-                this.page.keyboard.press("Enter");
-                this.page.locator('#years').nth(0).click();
-                this.page.keyboard.press("ArrowDown");
-                this.page.keyboard.press("Enter");
-                this.page.getByRole('checkbox', { name: "Sign up for our newsletter!" }).click();
-                this.page.getByRole('textbox', { name: 'First name' }).fill("Test");
-                this.page.getByRole('textbox', { name: 'Last name' }).fill("User");
-                this.page.getByRole('textbox', { name: "Address * (Street address, P.O. Box, Company name, etc.)" }).fill("Mogbazar, Dhaka");
-                this.page.getByRole('combobox', { name: "Country *" }).click();
-                this.page.keyboard.press("ArrowDown");
-                this.page.keyboard.press("Enter");
-                this.page.getByRole('textbox', { name: "State *" }).fill("England");
-                this.page.getByRole('textbox', { name: "City * Zipcode *" }).fill("London");
-                this.page.locator('#zipcode').fill('48');
-
-                this.page.getByRole('textbox', { name: "Mobile Number *" }).fill("88000008467");
-                this.page.getByRole('button', { name: "Create Account" }).click();
-                //expect(page.getByRole('heading', { name: "ACCOUNT CREATED" })).toHaveText("Account Created!");
-                //this.// page.getByRole('link',{name:"Continue"}).click();
-                this.page.getByText("Continue").click();
+        async registration(name: string, email: string) {
+                await this.page.getByText('Signup').click();
+                await this.page.getByRole('textbox', { name: 'Name' }).fill(name);
+                await this.page.locator('[name=email]').nth(1).fill(email);
+                await this.page.getByRole('button', { name: 'Signup' }).click();
+                await this.page.getByRole('radio', { name: 'Mrs.' }).click();
+                await this.page.getByRole('textbox', { name: 'Password' }).fill('testuser123');
+                await this.page.locator('#days').nth(0).click();
+                await this.page.keyboard.press("ArrowDown");
+                await this.page.keyboard.press("Enter");
+                await this.page.locator('#months').nth(0).click();
+                await this.page.keyboard.press("ArrowDown");
+                await this.page.keyboard.press("Enter");
+                await this.page.locator('#years').nth(0).click();
+                await this.page.keyboard.press("ArrowDown");
+                await this.page.keyboard.press("Enter");
+                await this.page.getByRole('checkbox', { name: "Sign up for our newsletter!" }).click();
+                await this.page.getByRole('textbox', { name: 'First name' }).fill("Test");
+                await this.page.getByRole('textbox', { name: 'Last name' }).fill("User");
+                await this.page.getByRole('textbox', { name: "Address * (Street address, P.O. Box, Company name, etc.)" }).fill("Mogbazar, Dhaka");
+                await this.page.getByRole('combobox', { name: "Country *" }).click();
+                await this.page.keyboard.press("ArrowDown");
+                await this.page.keyboard.press("Enter");
+                await this.page.getByRole('textbox', { name: "State *" }).fill("England");
+                await this.page.getByRole('textbox', { name: "City * Zipcode *" }).fill("London");
+                await this.page.locator('#zipcode').fill('48');
+                await this.page.getByRole('textbox', { name: "Mobile Number *" }).fill("88000008467");
+                await this.page.getByRole('button', { name: "Create Account" }).click();
+                await this.page.getByText("Continue").click();
         }
 }
 
-export {Signup}
+export { Signup }
--- tests/automationExerciseSignup.spec.ts
+++ tests/automationExerciseSignup.spec.ts
@@ -1,46 +1,49 @@
 import { test, expect } from "@playwright/test";
 import { faker } from '@faker-js/faker';
+import { Signup } from "../pages/signup";
 
 
 
 test("Sign up", async ({ page }) => {
     await page.goto('https://www.automationexercise.com/');
+    await expect(page.getByRole('heading', { name: "AutomationExercise" })).toHaveText("AutomationExercise");
 
-    //await expect(page.getByRole('heading',{name: "AutomationExercise"})).toHaveText("AutomationExercise");
-    await page.getByText('Signup').click();
-    await page.getByRole('textbox', { name: 'Name' }).fill("Test User");
-    await page.locator('[name=email]').nth(1).fill(faker.internet.email());
-    // page.getByRole('textbox',{name:"Email Address"}).fill("testuser005@yopmail.com");
-    await page.getByRole('button', { name: 'Signup' }).click();
-    await page.getByRole('radio', { name: 'Mrs.' }).click();
-    await page.getByRole('textbox', { name: 'Password' }).fill('testuser123');
-    // page.getByRole('combobox',{value:'Day'}).click();
-         // page.getByText("Day").click();
-    await page.locator('#days').nth(0).click();
-    await page.keyboard.press("ArrowDown");
-    await page.keyboard.press("Enter");
-    await page.locator('#months').nth(0).click();
-    await page.keyboard.press("ArrowDown");
-    await page.keyboard.press("Enter");
-    await page.locator('#years').nth(0).click();
-    await page.keyboard.press("ArrowDown");
-    await page.keyboard.press("Enter");
-    await page.getByRole('checkbox', { name: "Sign up for our newsletter!" }).click();
-    await page.getByRole('textbox', { name: 'First name' }).fill("Test");
-    await page.getByRole('textbox', { name: 'Last name' }).fill("User");
-    await page.getByRole('textbox', { name: "Address * (Street address, P.O. Box, Company name, etc.)" }).fill("Mogbazar, Dhaka");
-    await page.getByRole('combobox', { name: "Country *" }).click();
-    await page.keyboard.press("ArrowDown");
-    await page.keyboard.press("Enter");
-    await page.getByRole('textbox', { name: "State *" }).fill("England");
-    await page.getByRole('textbox', { name: "City * Zipcode *" }).fill("London");
-    await page.locator('#zipcode').fill('48');
+    const user = new Signup(page);
+    await user.registration(faker.person.firstName(), faker.internet.email())
 
-    await page.getByRole('textbox', { name: "Mobile Number *" }).fill("88000008467");
-    await page.getByRole('button', { name: "Create Account" }).click();
-    expect(page.getByRole('heading', { name: "ACCOUNT CREATED" })).toHaveText("Account Created!");
-    await page.getByRole('link',{name:"Continue"}).click();
-    await page.getByText("Continue").click();
+
+    // await page.getByText('Signup').click();
+    // await page.getByRole('textbox', { name: 'Name' }).fill("Test User");
+    // await page.locator('[name=email]').nth(1).fill(faker.internet.email());
+    // await page.getByRole('button', { name: 'Signup' }).click();
+    // await page.getByRole('radio', { name: 'Mrs.' }).click();
+    // await page.getByRole('textbox', { name: 'Password' }).fill('testuser123');
+    // await page.locator('#days').nth(0).click();
+    // await page.keyboard.press("ArrowDown");
+    // await page.keyboard.press("Enter");
+    // await page.locator('#months').nth(0).click();
+    // await page.keyboard.press("ArrowDown");
+    // await page.keyboard.press("Enter");
+    // await page.locator('#years').nth(0).click();
+    // await page.keyboard.press("ArrowDown");
+    // await page.keyboard.press("Enter");
+    // await page.getByRole('checkbox', { name: "Sign up for our newsletter!" }).click();
+    // await page.getByRole('textbox', { name: 'First name' }).fill("Test");
+    // await page.getByRole('textbox', { name: 'Last name' }).fill("User");
+    // await page.getByRole('textbox', { name: "Address * (Street address, P.O. Box, Company name, etc.)" }).fill("Mogbazar, Dhaka");
+    // await page.getByRole('combobox', { name: "Country *" }).click();
+    // await page.keyboard.press("ArrowDown");
+    // await page.keyboard.press("Enter");
+    // await page.getByRole('textbox', { name: "State *" }).fill("England");
+    // await page.getByRole('textbox', { name: "City * Zipcode *" }).fill("London");
+    // await page.locator('#zipcode').fill('48');
+
+    // await page.getByRole('textbox', { name: "Mobile Number *" }).fill("88000008467");
+    // await page.getByRole('button', { name: "Create Account" }).click();
+    // expect(page.getByRole('heading', { name: "ACCOUNT CREATED" })).toHaveText("Account Created!");
+    // await page.getByRole('link',{name:"Continue"}).click();
+    // await page.getByText("Continue").click();
     await page.pause();
+
 
 })
